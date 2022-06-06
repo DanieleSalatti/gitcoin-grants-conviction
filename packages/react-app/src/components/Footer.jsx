54df2be6ec84a5d26dcd7c54ae3881d8bda059cc
DanieleSalatti <me@danielesalatti.com>
2022-06-06 11:40:06 -0700

    Added footer

--- packages/react-app/src/components/Footer.jsx
+++ packages/react-app/src/components/Footer.jsx
@@ -5,21 +5,34 @@ import React from "react";
 
 export default function ScaffoldFooter() {
   const { Footer } = Layout;
-  return <></>;
   return (
     <Footer style={{ textAlign: "center", background: "transparent" }}>
-      Created by{" "}
-      <a href="https://twitter.com/DanieleSalatti" target="_blank">
-        Daniele Salatti
-      </a>{" "}
-      and the{" "}
-      <a href="https://buidlguidl.com" target="_blank">
-        BuidlGuidl
-      </a>{" "}
-      using{" "}
-      <a href="https://github.com/scaffold-eth/scaffold-eth" target="_blank">
-        🏗 scaffold-eth
-      </a>
+      <p>
+        Created by the{" "}
+        <a href="https://buidlguidl.com" target="_blank">
+          BuidlGuidl
+        </a>{" "}
+        using{" "}
+        <a href="https://github.com/scaffold-eth/scaffold-eth" target="_blank">
+          🏗 scaffold-eth
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/DanieleSalatti/gitcoin-grants-conviction" target="_blank">
+          code
+        </a>
+        {" / "}
+        <a href="https://etherscan.io/address/0x195acfcF9f06e43410a3ad177665F358E659cDA6#code" target="_blank">
+          contract (mainnet)
+        </a>
+        {" / "}
+        <a
+          href="https://optimistic.etherscan.io/address/0xbBb02E07bd83947e920D746b25a067B9424537b7#code"
+          target="_blank"
+        >
+          contract (optimism)
+        </a>
+      </p>
     </Footer>
   );
 }
